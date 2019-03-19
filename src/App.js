1cc8f2100fb2d6d42391ffdff4d5848aef2e1930
jmhart <jeffhart91@gmail.com>
2019-03-19 14:36:58 -0500

    Added font awesome 5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCloudSunRain } from "@fortawesome/free-solid-svg-icons";
 import Weather from "./components/Weather";
 import "./App.css";
+import "./weather.css";
+
+library.add(faCloudSunRain);
 
 class App extends Component {
   render() {
