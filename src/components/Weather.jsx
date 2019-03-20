9ccbbe648887341b8a7f16e8f93de9b761d9329a
jmhart <jeffhart91@gmail.com>
2019-03-20 09:48:25 -0500

    Implemented hourly weather

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -3,25 +3,30 @@ import WeatherCurrently from "./WeatherCurrently";
 import WeatherDaily from "./WeatherDaily";
 import {
   getCurrentWeather,
+  getHourlyWeather,
   getDailyWeather
 } from "../services/fakeWeatherService";
+import WeatherHourly from "./WeatherHourly";
 
 class Weather extends Component {
   state = {
     currently: {},
+    hourly: [],
     daily: []
   };
 
   componentDidMount() {
     const currently = getCurrentWeather();
+    const hourly = getHourlyWeather();
     const daily = getDailyWeather();
-    this.setState({ currently, daily });
+    this.setState({ currently, hourly, daily });
   }
 
   render() {
     return (
       <React.Fragment>
         <WeatherCurrently data={this.state.currently} />
+        <WeatherHourly data={this.state.hourly} />
         <WeatherDaily data={this.state.daily} />
       </React.Fragment>
     );
